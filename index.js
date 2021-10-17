d3134c06f90072258cad61b85e7ebbc581bea094
Ibrahim Abulaila <abeabulaila@gmail.com>
2021-10-16 20:43:42 -0400

    completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function add (x, y){
+         return x + y;
+}
+
+function subtract (a, b){
+         return a - b;
+}
+
+function multiply (a, b){
+        return a * b;
+}
+
+function divide (a, b){
+         return a / b;
+}
+
+function increment(n) {
+        return (n + 1);
+}
+
+function decrement(n) {
+        return (n - 1);
+}
+
+function makeInt(n) {
+    var result = parseInt(n, 10);
+    return result;
+}
+
+function preserveDecimal(n) {
+    var result = parseFloat(n);
+    return result;
+}
